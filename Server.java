4a9970cf8b9fe53506d69adccf9c3be7382f8499
mirzasamreen <samreen4995@gmail.com>
2016-08-23 11:55:27 +0530

    Add files via upload

--- Server.java
+++ Server.java
@@ -1,71 +1,111 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
- 
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
 public class Server
 {
- 
-    private static Socket socket;
- 
-    public static void main(String[] args)
+    public static void main(String args[]) throws Exception
+    {
+        ServerSocket soc=new ServerSocket(25000);
+        System.out.println("FTP Server is Connected.");
+       while(true)
+        {
+            System.out.println("Waiting for Connection ...");
+            transferfile t=new transferfile(soc.accept());
+            
+        }
+    }
+}
+
+class transferfile extends Thread
+{
+    Socket ClientSoc;
+
+    DataInputStream din;
+    DataOutputStream dout;
+    
+    transferfile(Socket soc)
     {
         try
         {
- 
-            int port = 25000;
-            ServerSocket serverSocket = new ServerSocket(port);
-            System.out.println("Server Started and listening to the port 25000");
- 
-            //Server is running always. This is done using this while(true) loop
-            while(true)
-            {
-                //Reading the message from the client
-                socket = serverSocket.accept();
-                InputStream is = socket.getInputStream();
-                InputStreamReader isr = new InputStreamReader(is);
-                BufferedReader br = new BufferedReader(isr);
-                String number = br.readLine();
-                System.out.println("Message received from client is "+number);
- 
-                //Multiplying the number by 2 and forming the return message
-                String returnMessage;
-                try
-                {
-                    int numberInIntFormat = Integer.parseInt(number);
-                    int returnValue = numberInIntFormat*2;
-                    returnMessage = String.valueOf(returnValue) + "\n";
-                }
-                catch(NumberFormatException e)
-                {
-                    //Input was not a number. Sending proper message back to client.
-                    returnMessage = "Please send a proper number\n";
-                }
- 
-                //Sending the response back to the client.
-                OutputStream os = socket.getOutputStream();
-                OutputStreamWriter osw = new OutputStreamWriter(os);
-                BufferedWriter bw = new BufferedWriter(osw);
-                bw.write(returnMessage);
-                System.out.println("Message sent to the client is "+returnMessage);
-                bw.flush();
-            }
+            ClientSoc=soc;                        
+            din=new DataInputStream(ClientSoc.getInputStream());
+            dout=new DataOutputStream(ClientSoc.getOutputStream());
+            System.out.println("Client Connected.");
+            start();
+            
         }
-        catch (Exception e)
+        catch(Exception ex)
         {
-            e.printStackTrace();
+        }        
+    }
+    void SendFile() throws Exception
+    {        
+        String filename=din.readUTF();
+        File f=new File(filename);
+       
+            FileInputStream fin=new FileInputStream(f);
+            int ch;
+            do
+            {
+                ch=fin.read();
+                dout.writeUTF(String.valueOf(ch));
+            }
+            while(ch!=-1);    
+            fin.close();                           
         }
-        finally
+    
+    
+    void ReceiveFile() throws Exception
+    {
+        String fileName=din.readUTF();
+        File f=new File(fileName);
+        
+             FileOutputStream fout=new FileOutputStream(f);
+                int ch;
+                String temp;
+                do
+                {
+                    temp=din.readUTF();
+                    ch=Integer.parseInt(temp);
+                    if(ch!=-1)
+                    {
+                        fout.write(ch);                    
+                    }
+                }while(ch!=-1);
+                fout.close();
+                dout.writeUTF("File has been sent.");
+            }
+            
+            
+    
+
+
+    public void run()
+    {
+        while(true)
         {
             try
             {
-                socket.close();
+            String Command=din.readUTF();
+            if(Command.compareTo("GET")==0)
+            {
+                SendFile();
+                continue;
             }
-            catch(Exception e){}
+            else if(Command.compareTo("SEND")==0)
+            {
+                ReceiveFile();
+                continue;
+            }
+            else if(Command.compareTo("DISCONNECT")==0)
+            {
+                System.exit(1);
+            }
+            }
+            catch(Exception ex)
+            {
+            }
         }
     }
 }
